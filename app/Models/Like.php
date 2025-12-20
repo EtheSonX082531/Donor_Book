c0c598574e85724fc0b535b91baa929455c00b5b
prianto dey <149568690+Deyprianto@users.noreply.github.com>
2025-12-20 17:59:51 +0600

    Enhance Like model with comments and documentation
    Added documentation and comments to the Like model.

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -1,24 +1,48 @@
 <?php
+
 // app/Models/Like.php
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Like Model
+ *
+ * This model represents a "like" made by a user on a post.
+ * It is typically used to track which users liked which posts.
+ */
 class Like extends Model
 {
+    // Enables factory support for testing and database seeding
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * These fields can be safely assigned using Like::create([...])
+     */
     protected $fillable = [
-        'user_id',
-        'post_id',
+        'user_id', // ID of the user who liked the post
+        'post_id', // ID of the post that was liked
     ];
 
+    /**
+     * Get the user who liked the post.
+     *
+     * Relationship: Like belongs to a User
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * Get the post that was liked.
+     *
+     * Relationship: Like belongs to a Post
+     */
     public function post()
     {
         return $this->belongsTo(Post::class);
